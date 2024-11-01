fe2d7aa1acbdc3e51a5e638939dac89d5a0b8c02
CoMakar <krikazon@mail.ru>
2024-11-01 23:45:05 +0700

    LR 4 JS code cleanup

--- src/script_4.js
+++ src/script_4.js
@@ -44,9 +44,14 @@ class Task_1 extends Task {
     }
 
     handle_button() {
+        if (!this.validate_inputs()) {
+            return;
+        }
+
         this.process_button.disabled = true;
         this.k_input.disabled = true;
         this.data_input.disabled = true;
+        
         this.process_data().finally(() => {
             this.process_button.disabled = false;
             this.k_input.disabled = false;
@@ -54,19 +59,20 @@ class Task_1 extends Task {
         });
     }
 
-    async process_data() {
+    // FIXME: separate validation from error showing
+    validate_inputs() {
         this.hide_error();
         let k_value = Number(this.k_input.value);
         let data_string = this.data_input.value;
 
         if (isNaN(k_value)) {
             this.show_error("Некорректное значение K");
-            return;
+            return false;
         }
 
         if (!DATA_STRING_PATTERN.test(data_string)) {
             this.show_error("Некорректный формат данных");
-            return;
+            return false;
         }
 
         let values = data_string
@@ -76,20 +82,32 @@ class Task_1 extends Task {
 
         if (values.at(-1) != 0) {
             this.show_error("Последовательность должна оканчиваться на 0");
-            return;
+            return false;
         }
 
         if (values.slice(0, -1).some((v) => v == 0)) {
             this.show_error(
                 "Последовательность не должна содержать нулевые значения"
             );
-            return;
+            return false;
         }
 
-        this.numbers_display.textContent = values.join("; ");
+        return true;
+    }
+
+    async process_data() {
+        let k_value = Number(this.k_input.value);
+        let data_string = this.data_input.value;
+
+        let values = data_string
+            .trim()
+            .split(DATA_SEPARATOR_PATTERN)
+            .map((v) => Number(v));
 
         let counter = 0;
         let found = false;
+
+        this.numbers_display.textContent = values.join("; ");
 
         for (let value of values) {
             counter++;
@@ -143,28 +161,39 @@ class Task_2 extends Task {
     }
 
     handle_button() {
+        if (!this.validate_inputs()) {
+            return;
+        }
+
         this.process_button.disabled = true;
         this.n_input.disabled = true;
+
         this.process_data().finally(() => {
             this.process_button.disabled = false;
             this.n_input.disabled = false;
         });
     }
 
-    async process_data() {
+    // FIXME: separate validation from error showing
+    validate_inputs() {
         this.hide_error();
         let n_value = Number(this.n_input.value);
 
         if (isNaN(n_value)) {
             this.show_error("Некорректное значение K");
-            return;
+            return false;
         }
 
         if (n_value <= 0) {
             this.show_error("N должно быть больше 0");
-            return;
+            return false;
         }
 
+        return true;
+    }
+
+    async process_data() {
+        let n_value = Number(this.n_input.value);
         let square_value = 0;
         const calc_ni = (n) => 2 * n - 1;
 
